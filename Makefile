0dc4e6680a0dafe8f4afebfa8b985ac5d48a97bd
Gorciu <gorciuyt@gmail.com>
2026-08-19 16:48:39 +0200

    first working version guys

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 all:
-	mkdir -p kernel/initramfs/lib64 kernel/initramfs/usr/lib
+	mkdir -p kernel/initramfs/lib64 kernel/initramfs/usr/lib kernel/devdata
 	gcc -std=c17 impl/doom.c -o kernel/initramfs/init
 	cp /lib64/ld-linux-x86-64.so.2 kernel/initramfs/lib64
 	cp /usr/lib/libc.so.6 kernel/initramfs/usr/lib
+	cp doom1.wad kernel/initramfs
 	make -C kernel run
--- impl/doom.c
+++ impl/doom.c
@@ -1,11 +1,30 @@
-#define DOOM_IMPLEMENTATION 
+#define DOOM_IMPLEMENTATION
+
+#define DOOM_IMPLEMENT_PRINT
+#define DOOM_IMPLEMENT_MALLOC
+#define DOOM_IMPLEMENT_FILE_IO
+#define DOOM_IMPLEMENT_EXIT
+
 #include "../PureDOOM.h"
+
+#include <string.h>
+
+int global_slop = 1;
+
+char* better_getenv(const char *name) {
+    if (strcmp(name, "HOME") == 0) {
+        return "";
+    }
+    return NULL;
+}
 
 int main(int argc, char** argv)
 {
+    doom_set_getenv(better_getenv);
     doom_init(argc, argv, 0);
     while (true)
     {
+        global_slop += 1;
         doom_update();
     }
 }
